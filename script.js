77673ed4b4c26a5c29434b39e8fa97ebe561c11d
fred <fredericnguyen777@gmail.com>
2021-12-14 18:33:52 -0500

    added function to ask user for the length of the grid. Redisplays grid after selection.

--- script.js
+++ script.js
@@ -51,5 +51,12 @@ function clearGrid() {  document.getElementById("grid").innerHTML = "" 
   hoverDraw();
 }
 
+function promptGrid() { 
+  gridSize = prompt("Enter a number of squares per side for the grid (less or equal to 100).") 
+  while (gridSize > 100) {gridSize = prompt("Enter a number of squares per side for the grid (less or equal to 100).")} 
+  // Display grid again by showing borders. 
+  for (let i = 0; i < blankSquare.length; i++) {blankSquare[i].style.borderStyle = "solid";}
+}
+
 gridSquares();
 hoverDraw();
